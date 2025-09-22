ac9c91065554744fd17caab3ebc90f233b5703b4
velllu <a>
2025-09-22 15:27:52 +0200

    Fixed editor text desync

--- js/editor.js
+++ js/editor.js
@@ -26,28 +26,39 @@ class Highlighter {
 
     /**
      * @param whats_parsing 0 for state, 1 for symbol and 2 for direction
+     * @returns an object, that has an "element" key containing the HTML tag that
+     * represents the element, and a "completed" key, that tells it wether the state was
+     * completed or if it stopped at half
      */
     parse_element(whats_parsing) {
         let string = "";
-
-        let char;
-        while (this.has_next()) {
-            let previous_char = this.previous_char();
-            char = this.next_char();
-
-            if ((char == "," || char == ")") && previous_char != "\\") {
-                break;
-            } else {
-                string += char;
-            }
-        }
-
         let class_name;
 
         switch (whats_parsing) {
             case 0: class_name = "state"; break;
             case 1: class_name = "symbol"; break;
             case 2: class_name = "direction"; break;
+        }
+
+        let char;
+        while (this.has_next()) {
+            let previous_char = this.previous_char();
+            char = this.next_char();
+
+            // If we encounter a newline, it means that the user it means that the user
+            // is still typing the state, so we go to the next line
+            if (char == "\n") {
+                return {
+                    "element": span_builder(string, class_name) + "<br>",
+                    "completed": false
+                };
+            }
+
+            if ((char == "," || char == ")") && previous_char != "\\") {
+                break;
+            } else {
+                string += char;
+            }
         }
 
         string = span_builder(string, class_name);
@@ -57,7 +68,7 @@ class Highlighter {
         else if (char == ")")
             string += span_builder(char, "parenthesis");
 
-        return string;
+        return { "element": string, "completed": true };
     }
 
     parse_comment() {
@@ -76,6 +87,27 @@ class Highlighter {
         return span_builder("#" + string, "comment");
     }
 
+    parse_state() {
+        let string = "";
+
+        string += span_builder("(", "parenthesis");
+
+        // A state is like this (STATE, CHARACTER, STATE, CHARACTER, DIRECTION), these
+        // numbers represent that
+        for (const i of [0, 1, 0, 1, 2]) {
+            let element = this.parse_element(i);
+
+            if (element.completed) {
+                string += element.element;
+            } else {
+                string += element.element;
+                return string;
+            }
+        }
+
+        return string;
+    }
+
     parse() {
         let returned_string = "";
 
@@ -92,12 +124,7 @@ class Highlighter {
                     break;
 
                 case "(":
-                    returned_string += span_builder("(", "parenthesis");
-                    returned_string += this.parse_element(0);
-                    returned_string += this.parse_element(1);
-                    returned_string += this.parse_element(0);
-                    returned_string += this.parse_element(1);
-                    returned_string += this.parse_element(2);
+                    returned_string += this.parse_state();
                     break;
 
                 case ")":
@@ -110,6 +137,7 @@ class Highlighter {
 
                 case "\n":
                     returned_string += "<br>";
+                    break;
 
                 default:
                     returned_string += span_builder(char, "invalid");
@@ -126,23 +154,23 @@ function span_builder(contents, class_) {
 }
 
 function highlight(code) {
-    return new Highlighter(code).parse();
+    return new Highlighter(code).parse() + "<br>";
 }
 
 const code = document.getElementById("code");
-const testing = document.getElementById("code-text");
+const syntax_highlighting = document.getElementById("code-text");
 
 code.addEventListener("input", () => {
-    testing.innerHTML = highlight(code.value);
+    syntax_highlighting.innerHTML = highlight(code.value);
 
-    testing.scrollTop = code.scrollTop;
-    testing.scrollLeft = code.scrollLeft;
+    syntax_highlighting.scrollTop = code.scrollTop;
+    syntax_highlighting.scrollLeft = code.scrollLeft;
 });
 
 code.addEventListener("scroll", () => {
-    testing.scrollTop = code.scrollTop;
-    testing.scrollLeft = code.scrollLeft;
+    syntax_highlighting.scrollTop = code.scrollTop;
+    syntax_highlighting.scrollLeft = code.scrollLeft;
 });
 
 // for when first loading the page
-testing.innerHTML = highlight(code.value);
+syntax_highlighting.innerHTML = highlight(code.value);
